735820729b85f3b99fe394b53cd39480ce45952c
rbaris <80262651+rbaris@users.noreply.github.com>
2022-05-19 02:14:14 +0300

    Add files via upload

---
+++ serialTerm/serialTerm/Form1.Designer.cs
@@ -0,0 +1,438 @@
+﻿
+namespace serialTerm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toppanel = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnDisconnect1 = new System.Windows.Forms.Button();
+            this.btnConnect1 = new System.Windows.Forms.Button();
+            this.lblStatus1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbRate1 = new System.Windows.Forms.ComboBox();
+            this.cbPort1 = new System.Windows.Forms.ComboBox();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.btnDisconnect2 = new System.Windows.Forms.Button();
+            this.btnConnect2 = new System.Windows.Forms.Button();
+            this.lblStatus2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbRate2 = new System.Windows.Forms.ComboBox();
+            this.cbPort2 = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.cbSeconds = new System.Windows.Forms.ComboBox();
+            this.cbFiles = new System.Windows.Forms.ComboBox();
+            this.lblFiles = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toppanel.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toppanel
+            // 
+            this.toppanel.BackColor = System.Drawing.Color.Gold;
+            this.toppanel.Controls.Add(this.label7);
+            this.toppanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.toppanel.Location = new System.Drawing.Point(0, 0);
+            this.toppanel.Name = "toppanel";
+            this.toppanel.Size = new System.Drawing.Size(666, 41);
+            this.toppanel.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("NSimSun", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(174, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(289, 19);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Welcome , my Serial Terminal";
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel.Controls.Add(this.btnDisconnect1);
+            this.panel.Controls.Add(this.btnConnect1);
+            this.panel.Controls.Add(this.lblStatus1);
+            this.panel.Controls.Add(this.label2);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.cbRate1);
+            this.panel.Controls.Add(this.cbPort1);
+            this.panel.Location = new System.Drawing.Point(22, 47);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(199, 164);
+            this.panel.TabIndex = 1;
+            // 
+            // btnDisconnect1
+            // 
+            this.btnDisconnect1.Location = new System.Drawing.Point(108, 130);
+            this.btnDisconnect1.Name = "btnDisconnect1";
+            this.btnDisconnect1.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect1.TabIndex = 6;
+            this.btnDisconnect1.Text = "Disconnect";
+            this.btnDisconnect1.UseVisualStyleBackColor = true;
+            this.btnDisconnect1.Click += new System.EventHandler(this.btnDisconnect1_Click);
+            // 
+            // btnConnect1
+            // 
+            this.btnConnect1.Location = new System.Drawing.Point(17, 130);
+            this.btnConnect1.Name = "btnConnect1";
+            this.btnConnect1.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect1.TabIndex = 5;
+            this.btnConnect1.Text = "Connect";
+            this.btnConnect1.UseVisualStyleBackColor = true;
+            this.btnConnect1.Click += new System.EventHandler(this.btnConnect1_Click);
+            // 
+            // lblStatus1
+            // 
+            this.lblStatus1.AutoSize = true;
+            this.lblStatus1.Location = new System.Drawing.Point(37, 114);
+            this.lblStatus1.Name = "lblStatus1";
+            this.lblStatus1.Size = new System.Drawing.Size(94, 13);
+            this.lblStatus1.TabIndex = 4;
+            this.lblStatus1.Text = "Connection Status";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port Number";
+            // 
+            // cbRate1
+            // 
+            this.cbRate1.FormattingEnabled = true;
+            this.cbRate1.Items.AddRange(new object[] {
+            "115200",
+            "57600",
+            "38400",
+            "19200",
+            "9600",
+            "1200"});
+            this.cbRate1.Location = new System.Drawing.Point(34, 83);
+            this.cbRate1.Name = "cbRate1";
+            this.cbRate1.Size = new System.Drawing.Size(121, 21);
+            this.cbRate1.TabIndex = 1;
+            // 
+            // cbPort1
+            // 
+            this.cbPort1.FormattingEnabled = true;
+            this.cbPort1.Location = new System.Drawing.Point(34, 37);
+            this.cbPort1.Name = "cbPort1";
+            this.cbPort1.Size = new System.Drawing.Size(121, 21);
+            this.cbPort1.TabIndex = 0;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panelBottom.Controls.Add(this.btnDisconnect2);
+            this.panelBottom.Controls.Add(this.btnConnect2);
+            this.panelBottom.Controls.Add(this.lblStatus2);
+            this.panelBottom.Controls.Add(this.label5);
+            this.panelBottom.Controls.Add(this.label4);
+            this.panelBottom.Controls.Add(this.cbRate2);
+            this.panelBottom.Controls.Add(this.cbPort2);
+            this.panelBottom.Location = new System.Drawing.Point(22, 217);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(199, 172);
+            this.panelBottom.TabIndex = 2;
+            // 
+            // btnDisconnect2
+            // 
+            this.btnDisconnect2.Location = new System.Drawing.Point(108, 133);
+            this.btnDisconnect2.Name = "btnDisconnect2";
+            this.btnDisconnect2.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect2.TabIndex = 6;
+            this.btnDisconnect2.Text = "Disconnect";
+            this.btnDisconnect2.UseVisualStyleBackColor = true;
+            this.btnDisconnect2.Click += new System.EventHandler(this.btnDisconnect2_Click);
+            // 
+            // btnConnect2
+            // 
+            this.btnConnect2.Location = new System.Drawing.Point(17, 133);
+            this.btnConnect2.Name = "btnConnect2";
+            this.btnConnect2.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect2.TabIndex = 5;
+            this.btnConnect2.Text = "Connect";
+            this.btnConnect2.UseVisualStyleBackColor = true;
+            this.btnConnect2.Click += new System.EventHandler(this.btnConnect2_Click);
+            // 
+            // lblStatus2
+            // 
+            this.lblStatus2.AutoSize = true;
+            this.lblStatus2.Location = new System.Drawing.Point(40, 112);
+            this.lblStatus2.Name = "lblStatus2";
+            this.lblStatus2.Size = new System.Drawing.Size(94, 13);
+            this.lblStatus2.TabIndex = 4;
+            this.lblStatus2.Text = "Connection Status";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Baud Rate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Port Number";
+            // 
+            // cbRate2
+            // 
+            this.cbRate2.FormattingEnabled = true;
+            this.cbRate2.Items.AddRange(new object[] {
+            "115200",
+            "57600",
+            "38400",
+            "19200",
+            "9600",
+            "1200"});
+            this.cbRate2.Location = new System.Drawing.Point(34, 78);
+            this.cbRate2.Name = "cbRate2";
+            this.cbRate2.Size = new System.Drawing.Size(121, 21);
+            this.cbRate2.TabIndex = 1;
+            // 
+            // cbPort2
+            // 
+            this.cbPort2.FormattingEnabled = true;
+            this.cbPort2.Location = new System.Drawing.Point(34, 32);
+            this.cbPort2.Name = "cbPort2";
+            this.cbPort2.Size = new System.Drawing.Size(121, 21);
+            this.cbPort2.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.AutoSize = true;
+            this.btnSave.Location = new System.Drawing.Point(262, 102);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(127, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save Changes";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(262, 66);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(127, 23);
+            this.btnPath.TabIndex = 4;
+            this.btnPath.Text = "Choose Path";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(262, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(381, 233);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // cbSeconds
+            // 
+            this.cbSeconds.FormattingEnabled = true;
+            this.cbSeconds.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cbSeconds.Location = new System.Drawing.Point(522, 73);
+            this.cbSeconds.Name = "cbSeconds";
+            this.cbSeconds.Size = new System.Drawing.Size(58, 21);
+            this.cbSeconds.TabIndex = 6;
+            this.cbSeconds.Text = "1";
+            this.cbSeconds.SelectedIndexChanged += new System.EventHandler(this.cbSeconds_SelectedIndexChanged);
+            // 
+            // cbFiles
+            // 
+            this.cbFiles.FormattingEnabled = true;
+            this.cbFiles.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cbFiles.Location = new System.Drawing.Point(522, 101);
+            this.cbFiles.Name = "cbFiles";
+            this.cbFiles.Size = new System.Drawing.Size(58, 21);
+            this.cbFiles.TabIndex = 7;
+            this.cbFiles.Text = "1";
+            // 
+            // lblFiles
+            // 
+            this.lblFiles.AutoSize = true;
+            this.lblFiles.Location = new System.Drawing.Point(407, 104);
+            this.lblFiles.Name = "lblFiles";
+            this.lblFiles.Size = new System.Drawing.Size(87, 13);
+            this.lblFiles.TabIndex = 9;
+            this.lblFiles.Text = "How many files ?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(433, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Save Settings";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Location = new System.Drawing.Point(407, 76);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(109, 13);
+            this.lblSeconds.TabIndex = 11;
+            this.lblSeconds.Text = "How many seconds ?";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\baris\\source\\repos\\serialTerm\\serialTerm\\bin\\Debug";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(666, 398);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblFiles);
+            this.Controls.Add(this.cbFiles);
+            this.Controls.Add(this.cbSeconds);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.toppanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toppanel.ResumeLayout(false);
+            this.toppanel.PerformLayout();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.panelBottom.ResumeLayout(false);
+            this.panelBottom.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel toppanel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnDisconnect1;
+        private System.Windows.Forms.Button btnConnect1;
+        private System.Windows.Forms.Label lblStatus1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbPort1;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Button btnDisconnect2;
+        private System.Windows.Forms.Button btnConnect2;
+        private System.Windows.Forms.Label lblStatus2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbPort2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.Timer timer2;
+        public System.Windows.Forms.ComboBox cbRate1;
+        public System.Windows.Forms.ComboBox cbRate2;
+        private System.Windows.Forms.ComboBox cbSeconds;
+        private System.Windows.Forms.ComboBox cbFiles;
+        private System.Windows.Forms.Label lblFiles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ serialTerm/serialTerm/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace serialTerm
+{
+    public partial class Form1 : Form
+    {
+
+        private SerialPort _serialPort;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Visible = false;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            
+
+            foreach (string port in ports)
+            {
+                cbPort1.Items.Add(port);
+                cbPort2.Items.Add(port);
+            }
+            if (ports.Count() == 0)
+            {
+                cbPort1.Enabled = false;
+                cbPort2.Enabled = false;
+            }
+        }
+
+        private void btnConnect1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            try
+            {
+                serialPort1.PortName = cbPort1.Text;
+                serialPort1.BaudRate = Convert.ToInt16(cbRate1.Text);
+
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.Open();
+                    
+                    lblStatus1.Text = "Connection established.";
+                    lblStatus1.ForeColor = Color.Green;
+                    richTextBox1.Visible = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                
+                lblStatus1.Text = "Connection could not be established";
+                lblStatus1.ForeColor = Color.Red;
+            }
+
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    richTextBox1.Visible = true;
+                    serialPort1.WriteLine(richTextBox1.Text);
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Port connection not found");
+            }
+        }
+
+        private void btnDisconnect1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            serialPort1.Close();
+            
+            lblStatus1.Text = "Connection terminated";
+            lblStatus1.ForeColor = Color.Red;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string info = (serialPort1.ReadExisting());
+                richTextBox1.Visible = true;
+                richTextBox1.Text = info.ToString();
+                System.Threading.Thread.Sleep(1200);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void btnConnect2_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+            try
+            {
+                serialPort2.PortName = cbPort2.Text;
+                serialPort2.BaudRate = Convert.ToInt16(cbRate2.Text);
+
+                if (!serialPort2.IsOpen)
+                {
+                    serialPort2.Open();
+                    
+                    lblStatus2.Text = "Connection established.";
+                    lblStatus2.ForeColor = Color.Green;
+                }
+            }
+            catch (Exception)
+            {
+
+               
+                lblStatus2.Text = "Connection could not be established";
+                lblStatus2.ForeColor = Color.Red;
+            }
+
+            try
+            {
+                if (!serialPort2.IsOpen)
+                {
+                    richTextBox1.Visible = true;
+                    serialPort2.WriteLine(richTextBox1.Text);
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(" Port connection not found");
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                string info = (serialPort2.ReadExisting());
+                richTextBox1.Visible = true;
+                richTextBox1.Text = info.ToString();
+                System.Threading.Thread.Sleep(1200);
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+        private void btnDisconnect2_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            serialPort2.Close();
+            
+            lblStatus2.Text = "Connection terminated";
+            lblStatus2.ForeColor = Color.Red;
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            richTextBox1.Visible = true;
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+          
+                int saveloop = Convert.ToInt32(cbFiles.SelectedIndex);
+                string fpath = folderBrowserDialog1.SelectedPath;
+                string fullpath = string.Concat(fpath + "\\" + DateTime.Now.ToString().Replace(':', '_'));
+             
+                for (int i = 0; i < saveloop+1; i++)
+                {
+                    richTextBox1.SaveFile(fullpath + @".rtf");
+                }    
+        }
+
+        private void cbSeconds_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int recordTime = Convert.ToInt32(cbSeconds.SelectedIndex);
+            double ms = Math.Pow(10, 3);
+            int ms_int = Convert.ToInt32(ms);
+            timer1.Interval = (recordTime + 1) * ms_int;
+            timer2.Interval = (recordTime+1) * ms_int;
+        }         
+    }
+}
